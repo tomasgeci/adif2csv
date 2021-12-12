5bd3dc9e0469eb8ba8d632bb7f0eaad02e292a5d
Tomas Geci <tomas.geci@gmail.com>
2021-12-12 10:55:21 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+npm install
+
+./bin/adif2csv.js -f adif_file.adi
+
+todo add station callsign
---
+++ bin/adif2csv.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+
+import {readFileSync, writeFile} from 'fs';
+import yargs from 'yargs';
+import {hideBin} from 'yargs/helpers'
+import chalk from 'chalk';
+import boxen from 'boxen';
+import {AdifParser} from 'adif-parser-ts';
+import {convertArrayToCSV} from 'convert-array-to-csv';
+
+const cliOptions = yargs(hideBin(process.argv))
+    .usage("Usage: -f <file>")
+    .version('1.0.0')
+    .option('f', {
+        alias: "file",
+        describe: "Path to ADIF file ",
+        type: "string",
+        demandOption: true
+    })
+    .argv
+
+const loadedMsg = chalk.white.bold(`Loaded ADIF file ${cliOptions.file}`)
+
+const boxenOptions = {
+    padding: 1,
+    margin: 1,
+    borderStyle: "round",
+    borderColor: "green",
+    backgroundColor: "#445544"
+};
+
+const msgBox = boxen(loadedMsg, boxenOptions)
+console.log(msgBox);
+
+try {
+    const adifFileContent = readFileSync(cliOptions.file, 'utf8');
+    let adifContent = AdifParser.parseAdi(adifFileContent);
+    let count = adifContent.records.length;
+    let arrayForCsv = [];
+
+    adifContent.records.forEach(record => {
+        arrayForCsv.push(
+            {
+                'call': record['call'],
+                'band': record['band'],
+                'mode': record['mode'],
+                'time_on': record['time_on'],
+                'qso_date': record['qso_date'],
+                'rst_rcvd': record['rst_rcvd'],
+                'rst_sent': record['rst_sent']
+            }
+        );
+    });
+
+    let options = {separator: ','};
+    const csv = convertArrayToCSV(arrayForCsv, options);
+
+    writeFile(cliOptions.file + '.csv', csv, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+
+    const finishedMsg = chalk.green.bold(`Converted ADIF file ${cliOptions.file} to ${cliOptions.file}.csv with ${count} QSO records`)
+
+    const msgBox = boxen(finishedMsg, boxenOptions)
+    console.log(msgBox);
+
+} catch (err) {
+    console.error(err)
+}
